ce778c8e27bed5ae029b6cf3e52bbeb94db669e9
ayush <ak4426280@gmail.com>
2025-08-25 02:17:50 +0530

    added ui

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 import express from 'express';
-const app= express();
+import path from 'path';
 
-app.get("/",(req,res)=>{
-    res.send("Hello jee")
-})
+const app = express();
 
-app.get("/test",(req,res)=>{
-    res.send("CI/CD testing")
-})
+// Serve static files from "public" folder
+app.use(express.static(path.join(process.cwd(), 'public')));
 
-app.get("/new-test",(req,res)=>{
-    res.send("new ci cd working");
-})
+// Routes for APIs
+app.get("/test", (req, res) => {
+    res.send("CI/CD testing");
+});
 
-app.get("/newRoute",(req,res)=>{
-    res.send("This is ci cd working fine");
-})
+app.get("/new-test", (req, res) => {
+    res.send("New CI/CD working");
+});
 
-app.listen(3000,'0.0.0.0',()=>{
-    console.log("app is running");
-})
+app.get("/newRoute", (req, res) => {
+    res.send("This is CI/CD working fine");
+});
+
+// Serve the main UI
+app.get("/", (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
+});
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log("App is running on port 3000");
+});
